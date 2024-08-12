dff001ed242f4d0cb071e0ba6a6f67e45a241c2a
reagan505 <reagangachanja6@gmail.com>
2024-08-12 12:38:11 +0300

    js-test

---
+++ questions.js
@@ -0,0 +1,16 @@
+var carName = "Volvo"
+console.log(carName);
+
+var x = 50
+console.log(x);
+
+var x = 5
+var y = 10
+var z = x + y;
+console.log(z);
+
+
+var name = "john"
+var lastName = "Doe"
+var age = "35"
+console.log(name, lastName, age);
